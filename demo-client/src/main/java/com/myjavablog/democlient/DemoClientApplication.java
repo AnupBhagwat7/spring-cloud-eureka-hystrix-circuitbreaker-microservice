7e328c974d5c0a0b534111492d28ea3ba00580a4
anupb <anup.bhagwat7@gmail.com>
2020-01-10 05:44:58 +0530

    Initial commit

---
+++ demo-client/src/main/java/com/myjavablog/democlient/DemoClientApplication.java
@@ -0,0 +1,33 @@
+package com.myjavablog.democlient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@EnableCircuitBreaker
+@EnableHystrix
+@EnableDiscoveryClient
+@SpringBootApplication
+public class DemoClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoClientApplication.class, args);
+	}
+}
+
+@Configuration
+class Config{
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+
+}
